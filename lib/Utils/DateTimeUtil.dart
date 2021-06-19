788607af1d6fa136c2db42ea4f8ec4007b1e68f5
Trang <ngoctrangptit@gmail.com>
2021-06-19 17:36:27 +0700

    Thêm read timestamp

--- lib/Utils/DateTimeUtil.dart
+++ lib/Utils/DateTimeUtil.dart
@@ -203,4 +203,32 @@ class DateTimeUtil {
     return dateTime
         .add(Duration(days: DateTime.daysPerWeek - dateTime.weekday));
   }
+
+  static String readTimestamp(int timestamp) {
+    var now = DateTime.now();
+    var format = DateFormat('HH:mm a');
+    var date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+    var diff = now.difference(date);
+    var time = '';
+
+    if (diff.inSeconds <= 0 || diff.inSeconds > 0 && diff.inMinutes == 0 || diff.inMinutes > 0 && diff.inHours == 0 || diff.inHours > 0 && diff.inDays == 0) {
+      time = format.format(date);
+      time = "Hôm nay";
+    } else if (diff.inDays > 0 && diff.inDays < 7) {
+      if (diff.inDays == 1) {
+        time = diff.inDays.toString() + ' Ngày trước';
+      } else {
+        time = diff.inDays.toString() + ' Ngày trước';
+      }
+    } else {
+      if (diff.inDays == 7) {
+        time = (diff.inDays / 7).floor().toString() + ' Tuần trước';
+      } else {
+
+        time = (diff.inDays / 7).floor().toString() + ' Tuần trước';
+      }
+    }
+
+    return time;
+  }
 }
