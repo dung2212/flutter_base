1fb6f14d778449954ee3bc54f41cc15b9666f6fa
Trang <ngoctrangptit@gmail.com>
2021-07-24 12:52:13 +0700

    Thêm datetime stamp

--- lib/Utils/DateTimeUtil.dart
+++ lib/Utils/DateTimeUtil.dart
@@ -232,7 +232,10 @@ class DateTimeUtil {
       } else {
         time = diff.inDays.toString() + ' Ngày trước';
       }
-    } else {
+    }else if (diff.inDays > 365){
+      // return "${(diff.inDays / 365).floor()} ${(diff.inDays / 365).floor() == 1 ? "year" : "years"} ago";
+      time = "${(diff.inDays / 365).floor()} inday ${diff.inDays}";
+    } else{
       if (diff.inDays == 7) {
         time = (diff.inDays / 7).floor().toString() + ' Tuần trước';
       } else {
@@ -242,5 +245,45 @@ class DateTimeUtil {
     }
 
     return time;
+  }
+  static String displayTimeAgoFromTimestamp(String timestamp) {
+    final year = int.parse(timestamp.substring(0, 4));
+    final month = int.parse(timestamp.substring(5, 7));
+    final day = int.parse(timestamp.substring(8, 10));
+    final hour = int.parse(timestamp.substring(11, 13));
+    final minute = int.parse(timestamp.substring(14, 16));
+
+    final DateTime videoDate = DateTime(year, month, day, hour, minute);
+    final int diffInHours = DateTime.now().difference(videoDate).inHours;
+
+    String timeAgo = '';
+    String timeUnit = '';
+    int timeValue = 0;
+
+    if (diffInHours < 1) {
+      final diffInMinutes = DateTime.now().difference(videoDate).inMinutes;
+      timeValue = diffInMinutes;
+      timeUnit = 'phút';
+    } else if (diffInHours < 24) {
+      timeValue = diffInHours;
+      timeUnit = 'giờ';
+    } else if (diffInHours >= 24 && diffInHours < 24 * 7) {
+      timeValue = (diffInHours / 24).floor();
+      timeUnit = 'ngày';
+    } else if (diffInHours >= 24 * 7 && diffInHours < 24 * 30) {
+      timeValue = (diffInHours / (24 * 7)).floor();
+      timeUnit = 'tuần';
+    } else if (diffInHours >= 24 * 30 && diffInHours < 24 * 12 * 30) {
+      timeValue = (diffInHours / (24 * 30)).floor();
+      timeUnit = 'tháng';
+    } else {
+      timeValue = (diffInHours / (24 * 365)).round();
+      timeUnit = 'năm';
+    }
+
+    timeAgo = timeValue.toString() + ' ' + timeUnit;
+    timeAgo += timeValue > 1 ? '' : '';
+
+    return timeAgo + ' trước';
   }
 }
