4f2341bed617668990d17741aa00cd115207104f
Phong <hoangphong143>
2021-09-07 17:31:06 +0700

    thêm format số lẻ chỉ nhận .

--- lib/Utils/TextFormatUtil.dart
+++ lib/Utils/TextFormatUtil.dart
@@ -6,7 +6,8 @@ import 'Util.dart';
 //Format nhận số thập phân đổi tất cả dấu phẩy thành dấu chấm
 class CommaTextInputFormatter extends TextInputFormatter {
   @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
     String truncated = newValue.text;
     TextSelection newSelection = newValue.selection;
 
@@ -24,6 +25,19 @@ class CommaTextInputFormatter extends TextInputFormatter {
 //WhitelistingTextInputFormatter.digitsOnly
 TextInputFormatter NumberTextInputFormatter() {
   return FilteringTextInputFormatter.allow(RegExp(r'[0-9]'));
+}
+
+//Chỉ nhận số có 1 dấu .
+TextInputFormatter NumberTextInputFormatterDoubleOnly() {
+  FilteringTextInputFormatter.allow(RegExp(r"[0-9.]"));
+  return TextInputFormatter.withFunction((oldValue, newValue) {
+    try {
+      final text = newValue.text;
+      if (text.isNotEmpty) double.parse(text);
+      return newValue;
+    } catch (e) {}
+    return oldValue;
+  });
 }
 // class NumberTextInputFormatter extends TextInputFormatter {
 //   @override
@@ -46,7 +60,8 @@ TextInputFormatter NumberTextInputFormatter() {
 // }
 
 //Giới hạn ký tự MaxLenght
-class LengthLimitingTextFieldFormatterFixed extends LengthLimitingTextInputFormatter {
+class LengthLimitingTextFieldFormatterFixed
+    extends LengthLimitingTextInputFormatter {
   LengthLimitingTextFieldFormatterFixed(int? maxLength) : super(maxLength);
 
   @override
@@ -54,7 +69,9 @@ class LengthLimitingTextFieldFormatterFixed extends LengthLimitingTextInputForma
     TextEditingValue oldValue,
     TextEditingValue newValue,
   ) {
-    if (maxLength != null && maxLength! > 0 && newValue.text.characters.length > maxLength!) {
+    if (maxLength != null &&
+        maxLength! > 0 &&
+        newValue.text.characters.length > maxLength!) {
       // If already at the maximum and tried to enter even more, keep the old
       // value.
       if (oldValue.text.characters.length == maxLength) {
@@ -70,25 +87,37 @@ class LengthLimitingTextFieldFormatterFixed extends LengthLimitingTextInputForma
 //định dạng tiền
 class CurrencyTextInputFormatter extends TextInputFormatter {
   @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
     // remove characters to convert the value to double (because one of those may appear in the keyboard)
-    String newText = newValue.text.replaceAll('.', '').replaceAll(',', '').replaceAll('_', '').replaceAll('-', '');
+    String newText = newValue.text
+        .replaceAll('.', '')
+        .replaceAll(',', '')
+        .replaceAll('_', '')
+        .replaceAll('-', '');
     String value = newText;
     int cursorPosition = newText.length;
     if (newText.isNotEmpty) {
       value = Util.intToPriceDouble(newText.toInt());
       cursorPosition = value.length;
     }
-    return TextEditingValue(text: value, selection: TextSelection.collapsed(offset: cursorPosition));
+    return TextEditingValue(
+        text: value,
+        selection: TextSelection.collapsed(offset: cursorPosition));
   }
 }
 
 //Định dạng số dương
 class PositiveNumbersTextInputFormatter extends TextInputFormatter {
   @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
     // remove characters to convert the value to double (because one of those may appear in the keyboard)
-    String newText = newValue.text.replaceAll('.', '').replaceAll(',', '').replaceAll('_', '').replaceAll('-', '');
+    String newText = newValue.text
+        .replaceAll('.', '')
+        .replaceAll(',', '')
+        .replaceAll('_', '')
+        .replaceAll('-', '');
     String value = newText;
     int cursorPosition = newText.length;
 
@@ -96,9 +125,13 @@ class PositiveNumbersTextInputFormatter extends TextInputFormatter {
       if (newValue.text.contains(",")) {
         newText = newValue.text.replaceFirst(RegExp(','), '.');
       }
-      value = newText.toDouble() == null ? "" : Util.doubleToString(newText.toDouble()!);
+      value = newText.toDouble() == null
+          ? ""
+          : Util.doubleToString(newText.toDouble()!);
       cursorPosition = value.length;
     }
-    return TextEditingValue(text: value, selection: TextSelection.collapsed(offset: cursorPosition));
+    return TextEditingValue(
+        text: value,
+        selection: TextSelection.collapsed(offset: cursorPosition));
   }
 }
