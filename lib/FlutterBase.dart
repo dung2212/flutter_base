4347c98a5d3ad79d54310a802fb0d9deb07d6692
DUNGLMCENPC\DungLMCenPC <lemminhdungltv@gmail.com>
2023-01-12 16:04:52 +0700

    add Translate

--- lib/FlutterBase.dart
+++ lib/FlutterBase.dart
@@ -4,15 +4,24 @@ import 'dart:ui' as ui;
 import 'package:FlutterBase/ThirdParty/AutoSize/auto_size.dart';
 import 'package:FlutterBase/Utils/DeviceUtil.dart';
 import 'package:FlutterBase/Utils/ScreenUtil.dart';
-
 class FlutterBase {
   static String keyEncrypt = "P@123123123";
+  static Function(String? value)? OnGetLanguage;
 
   static init() {
     //ScreenUtil.pixelRatio = AutoSize.getPixelRatio();
     ScreenUtil.heightTopSafeArea = ui.window.viewPadding.top / ScreenUtil.pixelRatio;
     ScreenUtil.heightBottomSafeArea = ui.window.viewPadding.bottom / ScreenUtil.pixelRatio;
     DeviceUtil.initData();
+  }
+
+  static String? translate(String? value)
+  {
+    if(OnGetLanguage != null)
+      {
+       return OnGetLanguage?.call(value);
+      }
+    return value;
   }
 
 }
