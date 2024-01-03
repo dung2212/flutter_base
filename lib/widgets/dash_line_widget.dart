665eb86005356c78634823d59be20f58e2f2dd06
Minh Dũng Lê <leminhdungltv@gmail.com>
2024-01-03 17:14:24 +0700

    thêm dashline vertical

--- lib/widgets/dash_line_widget.dart
+++ lib/widgets/dash_line_widget.dart
@@ -2,34 +2,65 @@ import 'package:flutter/material.dart';
 
 class DashLineWidget extends StatelessWidget {
   final Color color;
+  final bool isVertical;
 
-  const DashLineWidget({Key? key, this.color = Colors.grey});
+  const DashLineWidget({Key? key, this.color = Colors.grey, this.isVertical = false});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: CustomPaint(
-        painter: DashedLinePainter(color: this.color),
-      ),
-    );
+    if (isVertical) {
+      return SizedBox(
+        height: double.infinity,
+        child: CustomPaint(
+          painter: DashedLinePainter(
+            color: this.color,
+            isVertical: isVertical,
+          ),
+        ),
+      );
+    }
+    else {
+      return SizedBox(
+        width: double.infinity,
+        child: CustomPaint(
+          painter: DashedLinePainter(
+            color: this.color,
+            isVertical: isVertical,
+          ),
+        ),
+      );
+    }
   }
 }
 
 class DashedLinePainter extends CustomPainter {
   final Color color;
+  final bool isVertical;
 
-  DashedLinePainter({Key? key, required this.color});
+  DashedLinePainter({
+    Key? key,
+    required this.color,
+    required this.isVertical,
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
-    double dashWidth = 6, dashSpace = 4, startX = 0;
+    double dashWidth = 6, dashSpace = 4;
     final paint = Paint()
       ..color = this.color
       ..strokeWidth = 0.7;
-    while (startX < size.width - dashSpace) {
-      canvas.drawLine(Offset(startX, 0), Offset(startX + dashWidth, 0), paint);
-      startX += dashWidth + dashSpace;
+    if (isVertical) {
+      double startY = 0;
+      while (startY < size.height - dashSpace) {
+        canvas.drawLine(Offset(0, startY), Offset(0, startY + dashWidth), paint);
+        startY += dashWidth + dashSpace;
+      }
+    } else {
+      double startX = 0;
+      while (startX < size.width - dashSpace) {
+        canvas.drawLine(Offset(startX, 0), Offset(startX + dashWidth, 0), paint);
+        startX += dashWidth + dashSpace;
+      }
     }
   }
 
